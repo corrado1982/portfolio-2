bf7d86891801b3b1533059557875cdb9030af2f1
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2025-11-18 12:54:53 +0100

    added click out

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const worksAsButton = document.querySelector(".works-button");
 const subMenu = document.querySelector(".submenu");
+const main = document.querySelector("main");
 
+main.addEventListener("click", closeMenu);
 worksAsButton.addEventListener("click", openMenu);
 
 function openMenu() {
   subMenu.classList.toggle("show");
+}
+function closeMenu() {
+  console.log("di");
+  if (subMenu.classList.contains("show")) {
+    subMenu.classList.remove("show");
+  }
 }
